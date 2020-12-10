9af86942e862b5f314c2a6a8c6ec9c3a09af6e0c
john ramirez <jackrunners317@gmail.com>
2020-12-09 23:13:13 -0800

    added timeframe, driver and navigator, and notes on project.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-$.ajax()
+'use strict';
+function Gallery(photo) {
+  this.keyword = photo.keyword;
+  this.horns = photo.horns;
+  this.image_url = photo.image_url;
+  this.description = photo.description;
+  this.title = photo.title;
+}
+Gallery.prototype.render = function () {
+  let $photoClone = $('.photo-template').clone();
+  $('main').append($photoClone);
+  $photoClone.find('h2').text(this.title);
+  $photoClone.find('img').attr('src', this.image_url);
+  $photoClone.find('p').text(this.description);
+  $photoClone.removeAttr('class');
+};
+Gallery.json = () => {
+  const ajaxSettings = {
+    method: 'get',
+    dataType: 'json'
+  };
+  $.ajax('data/horns.json', ajaxSettings)
+    .then(data => {
+      data.forEach(item => {
+        let photo = new Gallery(item);
+        console.log(photo);
+        photo.render();
+      });
+    });
+};
+$(() => Gallery.json());
